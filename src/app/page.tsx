27ceb3c9a2aad576aa669007e9bb9c8cec9c8088
Miguel Zepeda <migg_18@live.com>
2025-04-03 19:01:44 -0700

    Updated the contact me on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="mailto:your-email@example.com" // Replace with your contact email
+            href="mailto:m.zepeda.dev@gmail.com" // Replace with your contact email
             target="_blank"
             rel="noopener noreferrer"
           >
